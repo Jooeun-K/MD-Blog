1bbe041573e2c617869b30d6f719c39fdc4d1b42
Jooeun-K <gangjoo93@gmail.com>
2022-10-10 15:55:43 +0900

    Feat: 리다이렉트 설정 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/about");
+  }, []);
   return <div>메인페이지 = 목록 페이지</div>;
 };
 
